70799e8674b0cc1ef15cef8bc7f3d15a83d1edcb
rahatcnits <rahatcnits@gmail.com>
2024-01-25 10:29:13 +0600

    uptime monitoring app upto github

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * Title: Uptime Monitoring Application
+ * Description: A RESTFul API to monitor up or down time of user defined links
+ * Author: Mohammad Rahat
+ * Date: 25/01/2024
+ */
+
+// dependencies
+const http = require("http");
+
+// app object - module scaffolding
+const app = {};
