1e979f6853ec38719d70bd3b9107ab78c780a161
rahatcnits <rahatcnits@gmail.com>
2024-01-27 10:41:53 +0600

    create server in umtime minitoring app

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ const http = require("http");
 
 // app object - module scaffolding
 const app = {};
+
+// configuration
+app.config = {
+  port: 3000,
+};
+
+// create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`listening to port ${app.config.port}`);
+  });
+};
+
+// handle request response
+app.handleReqRes = (req, res) => {
+  // response handle
+  res.end("Hello programmers");
+};
+
+// start the server
+app.createServer();
